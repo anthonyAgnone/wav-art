83d5b0e57554ab6d66b39399f5a4320052091257
Anthony Agnone <ada60@pitt.edu>
2018-11-07 18:54:21 -0700

    finished react transition group implimentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import Header from './components/navigation/Header';
 import LandingPage from './components/landingPage/LandingPage';
 import DashBoard from './components/dashBoard/DashBoard';
 import HowTo from './components/howTo/HowTo';
 import PrintView from './components/printView/PrintView';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './components/utility/assets/css/app.css';
 
-export default function App() {
+function App({ location }) {
 	return (
 		<div className="app">
 			<Header />
 			<div className="content">
-				<Switch>
-					<Route path="/" exact component={LandingPage} />
-					<Route path="/how-to-use" component={HowTo} />
-					<Route path="/dashboard" component={DashBoard} />
-					<Route path="/preview" component={PrintView} />
-				</Switch>
+				<TransitionGroup component={null}>
+					<CSSTransition
+						in={true}
+						appear={false}
+						key={location.key}
+						classNames="page-slide"
+						timeout={300}
+					>
+						<Switch location={location}>
+							<Route path="/" exact component={LandingPage} />
+							<Route path="/how-to-use" component={HowTo} />
+							<Route path="/dashboard" component={DashBoard} />
+							<Route path="/preview" component={PrintView} />
+							<Route path="/login" component={Login} />
+							<Route path="/register" component={Register} />
+						</Switch>
+					</CSSTransition>
+				</TransitionGroup>
 			</div>
 		</div>
 	);
 }
+
+export default withRouter(App);
